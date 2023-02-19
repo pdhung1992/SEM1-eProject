edbba3480ab3899aad6a688d803afc33c7819a7c
pdhung1992 <115760562+pdhung1992@users.noreply.github.com>
2023-02-20 03:32:49 +0700

    update 20feb

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  day: number = 0;
+  hour: number = 0;
+  min: number = 0;
+  sec: number = 0;
 
+  ngOnInit(){
+  }
+
+  x = setInterval(() => {
+    var endDate: any = new Date("2023-04-22 17:00:00");
+    var today: any = new Date();
+    var distance = endDate - today;
+    this.day = Math.floor(distance / (1000 * 60 * 60 * 24));
+    this.hour = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    this.min = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    this.sec = Math.floor(distance % (1000 * 60) / (1000));
+    if (distance < 0){
+      clearInterval(this.x);
+    }
+  }, 1000)
 }
